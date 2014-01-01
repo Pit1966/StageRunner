1db527143a0fce92b3e308e937b1449d09ec9d9b
Peter Steinmeyer <steinmeyer@stonechip.de>
2014-01-01 14:16:08 +0100

    object and temp files removed from repo

--- build-StageRunner-MyDesktop_Qt_5_2_0_GCC_64bit-Debug/main/configrev.h
+++
@@ -1,8 +0,0 @@
-# ifndef CONFIGREV_H
-# define CONFIGREV_H
-# define APPNAME "StageRunner"
-# define APPVERSION "0.1.1-r"
-# define DOCSDIR "/usr/share/stagerunner/documents"
-# define PLUGINDIR "/usr/lib/qt4/plugins/stagerunner"
-# define TRANSLATIONDIR "/usr/share/stagerunner/translations"
-# endif
--- build-StageRunner-MySamsAndroid_GCC_4_7_Qt_5_2_0-Release/android-build/bin/AndroidManifest.xml.d
+++
@@ -1,2 +0,0 @@
-/home/peter/work/petercoding/git_stagerunner/build-StageRunner-MySamsAndroid_GCC_4_7_Qt_5_2_0-Release/android-build/bin/AndroidManifest.xml : \
-/home/peter/work/petercoding/git_stagerunner/build-StageRunner-MySamsAndroid_GCC_4_7_Qt_5_2_0-Release/android-build/AndroidManifest.xml \
