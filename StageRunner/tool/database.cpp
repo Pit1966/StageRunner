e84e749832470ed9e739467c58ad87bef2337a30
Peter Steinmeyer <peter@steinmeyer.org>
2019-04-29 23:58:23 +0200

    Added GPL prefix to source files

--- StageRunner/tool/database.cpp
+++ StageRunner/tool/database.cpp
@@ -1,13 +1,25 @@
-/** @file database.cpp */
-/***************************************************************************
-*   Copyright (C) 2008-2012 QASS GmbH                                     *
-*                                                                         *
-*   Ulrich Seuthe <useuthe@qass.net>                                      *
-*   Peter Steinmeyer <steinmeyer@qass.net>                                *
-*   Christian Günther <c.guenther@qass.net>                               *
-*                                                                         *
-*  This file is part of the OPTIMIZER project                             *
-***************************************************************************/
+//=======================================================================
+//  StageRunner
+//  Multi platform software that controls sound effects, sound recordings
+//  and lighting systems on small to medium-sized stages
+//
+//  Copyright (C) 2013-2019 by Peter Steinmeyer (Pit1966 on github)
+//  (C) Copyright 2019 stonechip entertainment
+//
+//  This program is free software; you can redistribute it and/or
+//  modify it under the terms of the GNU General Public License
+//  as published by the Free Software Foundation; version 2 of
+//  the License, or (at your option) any later version.
+//
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with this program; if not, write to the Free Software
+//  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+//=======================================================================
 
 #include "database.h"
 #include "toolclasses.h"
