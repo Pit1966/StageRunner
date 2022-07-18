07d0e346b38ef5cfa3d8a714a90bfd70698fbf77
Peter Steinmeyer <ps@stonechip.de>
2022-07-18 12:07:13 +0200

    Fixed deadlock in qt 5.12

--- StageRunner/system/qt_versions.h
+++ StageRunner/system/qt_versions.h
@@ -9,7 +9,15 @@
 #if QT_VERSION < QT_VERSION_CHECK(5, 14, 0)
 
 #define QT_SKIP_EMPTY_PARTS QString::SkipEmptyParts
-typedef QMutex QRecursiveMutex;
+
+class QRecursiveMutex : public QMutex
+{
+public:
+	QRecursiveMutex()
+		: QMutex(QMutex::Recursive)
+	{}
+};
+
 
 #else
 
